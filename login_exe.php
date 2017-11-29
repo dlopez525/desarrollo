ca90bb60bfbede93eb9cfd26cd9fe0f74261bf3b
Daniel López Sánchez <dlopezs2009@hotmail.com>
2017-11-29 09:26:17 -0500

    acount enabled

--- login_exe.php
+++ login_exe.php
@@ -5,31 +5,34 @@ if(!empty($_POST['email_txt']) and !empty($_POST['pass_txt'])) {
   $conexion = new Conexion();
   $email = $_POST['email_txt'];
   $pass = Encrypt($_POST['pass_txt']);
-  $sql = $conexion->query("SELECT id_usuario,id_tipoUsuario FROM usuarios WHERE  email='$email' AND password='$pass' LIMIT 1;");
+  $sql = $conexion->query("SELECT id_usuario,id_tipoUsuario,cuenta FROM usuarios WHERE  email='$email' AND password='$pass' LIMIT 1;");
   if($conexion->rows($sql) > 0) {
-    if($_POST['sesion']) { ini_set('session.cookie_lifetime', time() + (60*60*24)); }
-    @session_start();
     $data = $conexion->recorrer($sql);
-    $_SESSION['app_id'] = $data[0];
-    $_SESSION['app_tipoU'] = $data[1];
 
-    switch ($_SESSION['app_tipoU']) {
-      case '1':
-        header('Location: '.URL.'admin/');
-        break;
+    if ($data[2] == "Enabled") {
+      if($_POST['sesion']) { ini_set('session.cookie_lifetime', time() + (60*60*24)); }
+      @session_start();
+      $_SESSION['app_id'] = $data[0];
+      $_SESSION['app_tipoU'] = $data[1];
 
-      case '2':
-        header('Location: '.URL.'user/');
-        break;
+      switch ($_SESSION['app_tipoU']) {
+        case '1':
+          header('Location: '.URL.'admin/');
+          break;
 
-      default:
-      $mensaje = "<div class='logMsg-dang'><p>Debes de Iniciar Sesión</p></div>";
+        case '2':
+          header('Location: '.URL.'user/');
+          break;
+
+        default:
+        $mensaje = "<div class='logMsg-dang'><p>Debes de Iniciar Sesión</p></div>";
+        header('Location: '.URL.'login.php?err='.$mensaje);
+          break;
+      }
+    } else {
+      $mensaje = "<div class='logMsg-dang'><p>Debes de activar tu cuenta para poder iniciar sesión.</p></div>";
       header('Location: '.URL.'login.php?err='.$mensaje);
-        break;
     }
-
-
-    // echo 1;
   } else {
     $mensaje = "<div class='logMsg-war'><p>Email o Contraseña Incorrectos</p></div>";
     header('Location: '.URL.'login.php?err='.$mensaje);
