dfb53d4e08b1f9b9a54a8ed368371f47bc394b2e
Daniel López Sánchez <dlopezs2009@hotmail.com>
2017-11-30 19:16:21 -0500

    productos

---
+++ user/productos.php
@@ -0,0 +1,59 @@
+<?php include 'overall/head.php'; ?>
+<body class="grayBG">
+  <?php include 'overall/header.php'; ?>
+  <?php include 'overall/menu.php'; ?>
+
+    <div id="productos">
+        <div class="productos">
+            <div class="productos__item">
+                <div class="productos__item__img">
+                    <img src="img/producto.jpeg" alt="">
+                </div>
+                <div class="productos__item__details">
+                    <p class="productos__item__details__titulo">Izumi set</p>
+                    <p class="productos__item__details__cate">Sushi</p>
+                    <p class="productos__item__details__price">$7.50</p>
+                    <button class="productos__item__details__add"><i class="icon-ios-cart-outline"></i></button>
+                </div>
+            </div>
+
+            <div class="productos__item">
+                <div class="productos__item__img">
+                    <img src="img/producto.jpeg" alt="">
+                </div>
+                <div class="productos__item__details">
+                    <p class="productos__item__details__titulo">Izumi set</p>
+                    <p class="productos__item__details__cate">Sushi</p>
+                    <p class="productos__item__details__price">$7.50</p>
+                    <button class="productos__item__details__add"><i class="icon-ios-cart-outline"></i></button>
+                </div>
+            </div>
+
+            <div class="productos__item">
+                <div class="productos__item__img">
+                    <img src="img/producto.jpeg" alt="">
+                </div>
+                <div class="productos__item__details">
+                    <p class="productos__item__details__titulo">Izumi set</p>
+                    <p class="productos__item__details__cate">Sushi</p>
+                    <p class="productos__item__details__price">$7.50</p>
+                    <button class="productos__item__details__add"><i class="icon-ios-cart-outline"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="total">
+        <div class="total">
+            <div class="total__text">
+                <p>BUY</p>
+            </div>
+            <div class="total__price">
+                <p>$18.5</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="../views/js/script.js"></script>
+</body>
+</html>
