91cc57bd6856e2cf06be4598891644f94934276b
Daniel López Sánchez <dlopezs2009@hotmail.com>
2017-12-03 16:51:34 -0500

    layout compras

---
+++ user/compras.php
@@ -0,0 +1,118 @@
+<?php include 'overall/head.php'; ?>
+<body class="grayBG">
+     <?php include 'overall/header.php'; ?>
+    <?php include 'overall/menu.php'; ?>
+
+    <div class="compras">
+       <div class="compras__item">
+            <div class="compras__item__header">
+                <p>Codigo de Compra: <span>PED_DFDF4</span></p>
+                <p class="estado">Estado: <span>Pendiente</span></p>
+                <p>Fecha de Compra: <span>03-12-2017</span></p>
+            </div>
+
+            <div class="compras__item__body">
+                <table border="1">
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Age</th>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                    </tr>
+                    <tr>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>94</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Total</td>
+                        <td>500.00</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="compras__item__footer">
+                <a href="#" class="compras__item__footer__link">Cancelar Compra</a>
+            </div>
+       </div>
+       <div class="compras__item">
+            <div class="compras__item__header">
+                <p>Codigo de Compra: <span>PED_DFDF4</span></p>
+                <p>Estado: <span>Pendiente</span></p>
+                <p>Fecha de Compra: <span>03-12-2017</span></p>
+            </div>
+
+            <div class="compras__item__body">
+                <table border="1">
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Age</th>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                    </tr>
+                    <tr>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>94</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Total</td>
+                        <td>500.00</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="compras__item__footer">
+                <a href="#" class="compras__item__footer__link">Cancelar Compra</a>
+            </div>
+       </div>
+
+       <div class="compras__item">
+            <div class="compras__item__header">
+                <p>Codigo de Compra: <span>PED_DFDF4</span></p>
+                <p>Estado: <span>Pendiente</span></p>
+                <p>Fecha de Compra: <span>03-12-2017</span></p>
+            </div>
+
+            <div class="compras__item__body">
+                <table border="1">
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Age</th>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                    </tr>
+                    <tr>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>94</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Total</td>
+                        <td>500.00</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="compras__item__footer">
+                <a href="#" class="compras__item__footer__link">Cancelar Compra</a>
+            </div>
+       </div>
+   </div>
+
+    <script src="../views/js/script.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+</body>
+</html>
